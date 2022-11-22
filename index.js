a6edde73e38a23bde28b1487f13f0a354e9efdcc
Andrew <andrew_3586@iprimus.com.au>
2022-11-23 10:40:36 +1100

    finished exercise

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor( Math.random() * 1000 ) + 1;
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 3, 4,20);
+
+
+
